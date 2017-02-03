b4e57e15d6dbd96ffe54cd6ad1471378121db88c
LordBees <LordBees@users.noreply.github.com>
2017-02-03 15:08:02 +0000

    added sp/mp button
    added sp/mp button quickly
    note some tkinter wins will open - is bug

--- Class_Win_MP_Main.py
+++ Class_Win_MP_Main.py
@@ -1,1 +1,3 @@
 ##main window for submitting a link to the server
+class Win_Main_MP:
+    pass
--- Ted_Net.py
+++ Ted_Net.py
@@ -1,6 +1,22 @@
 ##main initialisation
 ##http://bit.do/list-of-url-shorteners.php
 ##http://www.hongkiat.com/blog/url-shortening-services-the-ultimate-list/
-import Class_Win_Main
+import Class_Win_Main,Class_Win_MP_Main
+from tkinter import *
+## quick window for mp or sp
+root = Tk()
+root.title('TED Gamemode selector')
 
-Class_Win_Main.Win_Main()
+def Play_SP():
+    Class_Win_Main.Win_Main()
+def Play_MP():
+    Class_Win_MP_Main.Win_Main_MP()
+
+SP_Button = Button(root,text = 'Singleplayer',command = Play_SP)#Class_Win_Main.Win_Main)
+MP_Button = Button(root,text = 'Multiplayer',command = Play_MP)#Class_Win_MP_Main.Win_Main_MP)
+#Class_Win_Main.Win_Main()
+#Class_Win_Main.Win_Main_MP()
+
+SP_Button.pack()
+MP_Button.pack()
+root.mainloop()
