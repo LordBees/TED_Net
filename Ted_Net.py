1be719d8b6f4764fb978e99dec0c657f8d22cb57
LordBees <LordBees@users.noreply.github.com>
2017-02-07 12:42:46 +0000

    Add files via upload

--- Ted_Net.py
+++ Ted_Net.py
@@ -3,12 +3,35 @@
 ##http://www.hongkiat.com/blog/url-shortening-services-the-ultimate-list/
 import Class_Win_Main,Class_Win_MP_Lobby#,Class_Win_MP_Main
 from tkinter import *
+import os
 ## quick window for mp or sp
 root = Tk()
 root.title('TED Gamemode selector')
 #root.geometry('300x100')
 root.geometry('150x100')
 
+def fs_check():
+    #def menu_settings_default(self):##reset to default ALL files
+    files = ['SETTINGS.CFF','CUSTOM.CLF','history.dat']
+    #if messagebox.askokcancel(title = 'confirm WIPE',message = 'are you sure\nthis will reset the historyfile\ncustomfile and settings to default!'):
+    print('file(s) generated: ',end = '')
+    if  not os.path.isfile('SETTINGS.CFF'):
+        print(',SETTINGS.CFF',end = '')
+        f = open('SETTINGS.CFF','w')
+        f.write('0,0,0,,')
+        f.close()
+    if  not os.path.isfile('CUSTOM.CLF'):
+        print(',CUSTOM.CLF',end = '')
+        f = open('CUSTOM.CLF','w')
+        f.write('https://www.youtube.com/watch?v=,10,10,0.1.2.3.4.5.6.7.8.9.A.B.C.D.E.F.G.H.I.J.K.L.M.N.O.P.Q.R.S.T.U.V.W.X.Y.Z.a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z..,1,,')
+        f.close()
+    if  not os.path.isfile('history.dat'):
+        print(',history.dat',end = '')
+        f = open('history.dat','w')
+        f.write('The History File')
+        f.close()##use mega for packing a backup?
+    print(' ...file ops complete')##gen
+    
 def Play_SP():
     root.destroy()
     Class_Win_Main.Win_Main()
@@ -28,6 +51,7 @@ D_label = Label(root,text = 'TED Gamemode selector')
 D_label.pack()#description label
 SP_Button.pack()#sp button
 MP_Button.pack()#mp button
+fs_check()
 root.mainloop()
 
 
