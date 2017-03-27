d9f70d107c601d53f0766bff898217845b5bfddb
LordBees <LordBees@users.noreply.github.com>
2017-03-27 16:22:01 +0100

    added help
    added help text to mp help box

--- Class_Win_MP_Howto.py
+++ Class_Win_MP_Howto.py
@@ -9,12 +9,35 @@ class Window:
         self.Drop_help_facts()
 
     def Drop_help_facts(self):
-        helptext = ['How to use LinkRoulette - Party v1'
+        helptext = ['How to use LinkRoulette - Party v1',
+                    '',
+                    '',
+                    'step 1:',
+                    'Hosting a session:',
+                    'to host a session you must click the \'host game\' button ',
+                    'in the top left corner of the window, you then should ',
+                    'click the button labelled\'set as admin of session\' ',
+                    'then close the window.',
+                    'your main window should now have an admin pin at the bottom',
+                    'instead of NONE.',
+                    'note down your game pin to give to other players later',
+                    'input a unique name into your username box',
+                    'then,click join to start the lobby',
+                    ' the other players can now join your session!',
+                    '(SEE- joining a session to see how)',
+                    '',
+                    'Joining a session:',
+                    'Enter a Username in the box provided',
+                    'enter a game pin for a session',
+                    'click join to join the game',
+                    '',
+                    '',
+                    ''
                     ] ##may want a seperate file for help
         ##helpwin = TK.Tk()##debigging win as seperate entity
         helpwin = TK.Toplevel()
-        helpwin.title("useful information")
-        helpwin.geometry('320x480')
+        helpwin.title("useful information (MP help)")
+        helpwin.geometry('325x480')
         helpwin_lblframe = TK.LabelFrame(helpwin, text="User Guide for MP")
         helpwin_lblframe.pack(fill="both", expand="yes")
      
