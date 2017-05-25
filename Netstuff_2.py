f34a9e325a5cca17fe7f0e58ad93925229476dd8
LordBees <LordBees@users.noreply.github.com>
2017-05-25 11:44:43 +0100

    Update Netstuff_2.py

--- Netstuff_2.py
+++ Netstuff_2.py
@@ -1,2 +1,9 @@
 ##import
 ##file for network stuff
+def scrapefrontpage():
+  pass
+def scrape_all(xurl,charset,linklen):
+  for x in range(0,linklen):
+    for y in range(0,len(charset)):
+      ##xurl
+      pass
