286c9a9d85de649f51eb23fa8a09d8c8e749a286
LordBees <LordBees@users.noreply.github.com>
2017-05-25 11:59:54 +0100

    Update Netstuff_2.py

--- Netstuff_2.py
+++ Netstuff_2.py
@@ -1,5 +1,7 @@
 ##import
 ##file for network stuff
+import BeeLibv3 as Blib
+
 def scrapefrontpage():
   pass
 def is_valid(link):
@@ -11,4 +13,26 @@ def scrape_all(xurl,charset,linklen):
     for y in range(0,len(charset)):
       ##xurl
         link = xurl+0
-        
+
+def getlinksvalid(type,linknum):
+  if linknum == -1:
+    ##get all
+  else:
+    get specific
+    
+ class linkcache:
+  ##attr
+  linktype2op = ""
+  cachex = []
+  
+  ##meth
+  def __init__(linktype2op):
+    self.linktype2op = linktype2op
+  
+  def cacheall():
+    if len(cachex)!=0:
+      cachex = []
+    
+    for x in range(len)
+  def cachelink():
+        pass
