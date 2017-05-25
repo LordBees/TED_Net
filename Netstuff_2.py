352f26e6d635087a997636b231225b0ecbbd32cc
LordBees <LordBees@users.noreply.github.com>
2017-05-25 11:53:15 +0100

    Update Netstuff_2.py

--- Netstuff_2.py
+++ Netstuff_2.py
@@ -5,9 +5,10 @@ def scrapefrontpage():
 def is_valid(link):
     pass
 def recurselooper(x,dat):
-    
+    pass
 def scrape_all(xurl,charset,linklen):
   for x in range(0,linklen):
     for y in range(0,len(charset)):
       ##xurl
-        link = xurl+
+        link = xurl+0
+        
