46abe4b1f5b9ce0da075e904533991fd55a02ca7
LordBees <LordBees@users.noreply.github.com>
2017-05-25 11:52:48 +0100

    added netstuff 2
    this is for utility net functions eg scraping front page for valid links for extra options  and brute forcing links into a LINKERS file

--- Netstuff_2.py
+++ Netstuff_2.py
@@ -2,8 +2,12 @@
 ##file for network stuff
 def scrapefrontpage():
   pass
+def is_valid(link):
+    pass
+def recurselooper(x,dat):
+    
 def scrape_all(xurl,charset,linklen):
   for x in range(0,linklen):
     for y in range(0,len(charset)):
       ##xurl
-      pass
+        link = xurl+
